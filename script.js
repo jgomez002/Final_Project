c850639e14129b8d3cc68a6591bfde54efe58b92
jgomez002 <jdg8858@nyu.edu>
2022-05-03 07:44:41 -0400

    update

--- script.js
+++ script.js
@@ -1,78 +1,81 @@
+// AOS js libary for fade in effect: normal edu site
+ AOS.init();
+ // JS fade out effect: for decoy site
+ // modified code from: https://codepen.io/bstonedev/pen/MWWZgKz
+ let elementsArray = document.querySelectorAll(".decoyscroll");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeOut );
+window.addEventListener('scroll', fadeIn );
+function fadeOut() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+}
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distoutView = elem.getBoundingClientRect().top + window.innerHeight + 20;
+        if (distoutView < 0) {
+            elem.classList.add("outView");
+        } else {
+            elem.classList.remove("outView");
+        }
+    }
+}
+fadeOut();
+fadeIn();
+
 function backButton(){
   window.history.back();
 }
 
-let outputElement = document.getElementById('outputElement');
-let outputParagraph = document.getElementById('outputParagraph');
-let contentGridElement = document.getElementById('contentGrid');
+
+// window.onscroll = function() {myFunction()};
+//
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+//
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
 
-let jsonDatabase = [
 {
-"title": "Pop",
-"bg": "#FFAAD2",
-"Artist": ["Ariana Grande", "Harry Styles", "Oliva Rodrigo"],
-"link": ["https://open.spotify.com/playlist/37i9dQZF1DWUa8ZRTfalHk?si=525ca6e9ab224d45"]
-},
-{
-"title": "Indie",
-"bg": "#8cba9c",
-"Artist": ["Omar Apollo", "TV Girl", "Clario"],
-"link": ["https://open.spotify.com/playlist/37i9dQZF1DX2Nc3B70tvx0?si=f2a599c05e344ba3"]
-},
-{
-"title": "Latin Rock/Indie",
-"bg": "#ff8680",
-"Artist": ["The Marias", "Senor Kino", "Los Blenders"],
-"link": ["https://open.spotify.com/playlist/17zEu9C6GgDVWrDsRPmazN?si=58c77a5228d44b57"]
-},
-{
-"title": "R&B",
-"bg": "#91BAD6",
-"Artist": ["Ella Mia", "The Weekend", "Brent Faiyaz"],
-"link": ["https://open.spotify.com/playlist/37i9dQZF1DX4SBhb3fqCJd?si=544e3142b54a4ba6"]
-},
-{
-"title": "Classical Music",
-"bg": "#C1BBDD",
-"Artist": ["Chopin", "English Chamber Orchestra", "Tchaikovsky"],
-"link": ["https://open.spotify.com/playlist/6fLmwfiN0zd93dSpRvfNU9?si=d9d90c194bbe4c2e"]
-},
-{
-"title": "Random",
-"bg": "#ab7c98",
-"Artist": ["???", "???", "??!!?"],
-"link": ["https://open.spotify.com/playlist/6ZZDxSRlFWJk3jybDLM2Ct?si=07968606a50e4e7c", "https://open.spotify.com/playlist/37i9dQZF1DX8C9xQcOrE6T?si=2eae343aea294b35", "https://www.youtube.com/watch?v=5qap5aO4i9A", "https://open.spotify.com/playlist/2uyBEC9ltSjlZWJGxEmGYa?si=dd140834364b4788", "https://www.youtube.com/watch?v=sWtEYPva4A0","https://www.youtube.com/watch?v=iIaRw59o_7g","https://youtu.be/iVg30U6O764","https://youtu.be/6RtEozldYkg","https://youtu.be/-wuU_3GmGWs","https://youtu.be/Lq2pt_1Y6eQ", "https://www.youtube.com/watch?v=ZZ5LpwO-An4&list=PLuOKYZONWsc6AfiJFfuoatAY59HbV2B44", "https://youtu.be/NT3yzVrp4o8","https://youtu.be/ZyoatfzxQ9s"]
-},
-];
+var app = document.getElementById('app');
 
-for (var i = 0; i < jsonDatabase.length; i++) {
-createElementProper(jsonDatabase[i]);
+
+var typewriter = new Typewriter(app,{
+    loop: false
+
+});
+
+typewriter.typeString('Take Time To Understand The Digital Inequalities That Exacerbate:')
+  .start();
 }
 
-
-// incoming js vars and json datasets css styled //
-// setting up divs
-function createElementProper(incomingJSON){
-let newContentElement = document.createElement("DIV");
-newContentElement.style.backgroundColor = incomingJSON['bg'];
-newContentElement.classList.add('contentItem');
-// Nested Title
-let newContentHeading = document.createElement("H3");
-newContentHeading.innerText = incomingJSON['title'];
-newContentElement.appendChild(newContentHeading);
-
-let newContentPara = document.createElement("H3");
-newContentPara.innerText = incomingJSON['Artist'];
-newContentElement.appendChild(newContentPara);
-
-let newContentLink = document.createElement("A");
-var randomIndex = Math.floor(Math.random() * incomingJSON['link'].length);
-function printToPage(incoming){
-newContentLink.innerText = incoming;
+function rotateElem() {
+            document.querySelector('.container').style.transform
+                       = 'rotate(90deg)';
 }
-printToPage(incomingJSON['link'][randomIndex]);
-newContentElement.appendChild(newContentLink);
 
-contentGridElement.appendChild(newContentElement);
-}
+var appear = document.getElementById('appear');
+
+// function appear(){
+// alert('need help??!, press the "o" in leo, its a shade darker');
+// }
+//
+// setTimeout(function (appear) {
+//
+// }, 30s);
+//
+// appear();
